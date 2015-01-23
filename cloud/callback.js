fb4b629e480ec8fcee6a5fd5ce11bcae26e9f9b2
Arun Suresh <arunvsuresh@gmail.com>
2015-01-23 15:04:47 -0800

    Add callback.js

---
+++ cloud/callback.js
@@ -0,0 +1,16 @@
+// var express = require('express');
+
+// var app = express();
+
+// //app.use(express.bodyParser());
+
+// // post to a callback url once the button has been clicked
+
+// module.exports
+
+// // tell coinbase where to post to
+// app.post('parse database', function(req, res){
+// 	console.log(req, res);
+// });
+
+// app.listen();
