f15c306e97e7678d1d49bc92abd6acb9c2b64d24
Luke Davis <lukefable@gmail.com>
2015-01-23 23:35:41 +0000

    moved all the mandrill logic back to main js

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,30 @@
-var mailboy = require('cloud/mailboy.js');
+var creds = require('cloud/creds.js');
+var Mandrill = require('mandrill');
+Mandrill.initialize(creds.mandrill() );
 
-Parse.job("sendEmail",mailboy.send);
+Parse.Cloud.job("sendMail",function(request,response){
+  Mandrill.sendEmail({
+    message: {
+      text: "Hello World!",
+      subject: "Using Cloud Code and Mandrill is great!",
+      from_email: "parse@cloudcode.com",
+      from_name: "Cloud Code",
+      to: [
+        {
+          email: "lucaswadedavis@gmail.com",
+          name: "Your Name"
+        }
+      ]
+    },
+    async: true
+  },{
+    success: function(httpResponse) {
+      console.log(httpResponse);
+      response.success("Email sent!");
+    },
+    error: function(httpResponse) {
+      console.error(httpResponse);
+      response.error("Uh oh, something went wrong");
+    }
+  });
+});
