8948c80681b77a056682baed8f619c4a77672f0b
Todd Skinner <todd.skinner@gmail.com>
2015-01-23 16:12:35 -0800

    Add console data in callback

--- cloud/callback.js
+++ cloud/callback.js
@@ -6,6 +6,8 @@ module.exports = function(req, res){
   var custEmail = data.customer.email;
 
   if (orderStatus === "complete") {
+    console.log(data);
+
     // trigger faux-poe
     // send file to custEmail
     return res.send(???);
