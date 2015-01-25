2eb8f479c1957cf9e37f8dad5a2148484dbc21e1
Arun Suresh <arunvsuresh@gmail.com>
2015-01-24 16:56:14 -0800

    Adding basicAuth to app.js

--- cloud/app.js
+++ cloud/app.js
@@ -3,11 +3,11 @@ var express = require('express');
 
 var app = express();
 app.use(express.bodyParser());
-app.post('/test', express.basicAuth('creds.parseUsername', 'creds.parsePassword'),  function(req, res) {
+app.post('/test', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
   res.json({ message: req.body.message });  // Send a JSON response
 });
 
-app.get('/test', function(req, res) {
+app.get('/test', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
   res.send("success"); // Send a JSON response
 });
 
