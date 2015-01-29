2560ba09d2221fb567e61bea589c95f795419bed
Luke Davis <lukefable@gmail.com>
2015-01-29 06:44:17 +0000

    adds lots of comments to explain the details of the next step and imports app.js into main.js.

--- cloud/app.js
+++ cloud/app.js
@@ -1,20 +1,31 @@
-var creds = require('cloud-creds');
 var express = require('express');
+var creds = require('cloud/creds');
+var fauxPoe = require('cloud/faux-poe.js');
+var Mandrill = require('mandrill');
+Mandrill.initialize(creds.mandrill );
 
 var app = express();
 app.use(express.bodyParser());
-app.post('/test', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
-  res.json({ message: req.body.message });  // Send a JSON response
+
+app.post('/cburl', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
+  //in order to inspect the structure of the JSON returned by Coinbase, we'll log it here
+  console.log(JSON.stringify(req.body.message));
+  //responds with JSON, I think this may eventually need to change to a status 200
+  res.json({ message: req.body.message });
+  //here in the near future - once we're sure this is working - we'll pull in
+  //the mandrill functionality that's currently in main.js
+  
+  //we'll harvest the email address of the customer from the req.body.message
+  
+  //inject it into the appropriate spot of the mandrill api
+  
+  //and send the email.
 });
 
-app.get('/test', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
-  res.send("success"); // Send a JSON response
+app.get('/cburl', express.basicAuth(creds.parseUsername, creds.parsePassword), function(req, res) {
+  //we'll remove this soon, it's mostly to test things, because curling with 
+  //GET is easier than curling with POST
+  res.send("success");
 });
-
-/* 
-app.get('/cburl', function(req, res){
-  res.send(200);
-});
-*/
 
 app.listen();
