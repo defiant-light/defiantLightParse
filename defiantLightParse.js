65ea6c8060afb2138ab2ea3ee10f006af8252244
Arun Suresh <arunvsuresh@gmail.com>
2015-02-02 11:43:27 -0800

    Adds yeoman for client-side testing.

---
+++ defiantLightParse.js
@@ -0,0 +1,6 @@
+/*! defiantLightParse v0.0.0 - MIT license */
+'use strict';
+
+var defiantLightParse = function () {
+  // your code goes here
+}
---
+++ spec/defiantLightParse.js
@@ -0,0 +1,20 @@
+/* global defiantLightParse, describe, it, expect, should */
+
+describe('defiantLightParse()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(defiantLightParse).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
