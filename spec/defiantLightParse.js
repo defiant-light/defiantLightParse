00e22faad74bea7a0aec548e9e46c55f098799cd
Todd Skinner <todd.skinner@gmail.com>
2015-02-02 12:26:00 -0800

    Add first tests

--- spec/defiantLightParse.js
+++ spec/defiantLightParse.js
@@ -1,10 +1,29 @@
 /* global defiantLightParse, describe, it, expect, should */
 
-describe('defiantLightParse()', function () {
+describe('chance()', function () {
   'use strict';
 
   it('exists', function () {
-    expect(defiantLightParse).to.be.a('function');
+    expect(Chance).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
+
+describe('mailboy()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(mailboy).to.be.a('function');
 
   });
 
